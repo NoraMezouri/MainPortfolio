9dc6f09fef1595e00ea9036905c7b566735926d2
NoraMezouri <156891253+NoraMezouri@users.noreply.github.com>
2024-05-16 08:26:54 +0200

    dsfds

--- typeEffect.js
+++ typeEffect.js
@@ -1,13 +1,18 @@
 let counter = 0
-let txt = 'webudvikler'
+let txtWU = 'webudvikler'
+let txtDM = 'Digital media elev'
 let speed = 80
+let comb = txtDM + txtWU
 
-function redTypeWriter() {
-  if (counter < txt.length) {
-    document.querySelector("#redType").innerHTML += txt.charAt(counter)
+function TypeWriter() {
+  if (counter < comb.length) {
+    document.querySelector("#redType").innerHTML += txtWU.charAt(counter)
+    document.querySelector("#blueType").innerHTML += txtDM.charAt(counter)
     counter++
-    setTimeout(redTypeWriter, speed)
+    setTimeout(TypeWriter, speed)
+    
   }
 }
 
 
+// document.querySelector("#blueType").innerHTML += txtDM.charAt(counter)
